0afd410d176c3d43ad18dfdc00d5ee29e61fb5af
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-11 04:59:07 +0300

    Add user and posts models

--- app/__init__.py
+++ app/__init__.py
@@ -23,12 +23,12 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
 
-    # Registering the blueprint
+    # Registering the blueprints
     from .main import main as main_blueprint
-    # from .auth import auth as auth_blueprint
-
     app.register_blueprint(main_blueprint)
-    # app.register_blueprint(auth_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     
     return app
 
