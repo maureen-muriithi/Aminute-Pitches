602d4c70f29c19568f6d4ad85fad7b5b7e1a0b75
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-12 12:03:05 +0300

    Update the pitches section and the comments section

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from . import main
 from flask_login import login_required, current_user
 from ..models import User, Pitch, Comment, Upvote, Downvote
 from .forms import UpdateProfile,PitchForm,CommentForm
-from .. import db,photos
+from .. import db
 
 @main.route('/')
 def index():
@@ -22,4 +22,42 @@ def pitches():
     upvote = Upvote.query.all()
     user = current_user
     return render_template('view_pitches.html', pitches=pitches, upvote=upvote, user=user)
+
+@main.route('/new_pitch', methods=['GET', 'POST'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user._get_current_object().id
+        post_obj = Pitch(post=post, title=title, category=category, user_id=user_id)
+        post_obj.save()
+        return redirect(url_for('main.index'))
+    return render_template('pitch.html', form=form)
+
+
+@main.route('/comment/<int:pitch_id>', methods=['GET', 'POST'])
+@login_required
+def comment(pitch_id):
+    form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    user = User.query.all()
+    comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        pitch_id = pitch_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(
+            comment=comment,
+            pitch_id=pitch_id,
+            user_id=user_id
+        )
+        new_comment.save()
+        new_comments = [new_comment]
+        print(new_comments)
+        return redirect(url_for('.comment', pitch_id=pitch_id))
+    return render_template('comment.html', form=form, pitch=pitch, comments=comments, user=user)
+
     
